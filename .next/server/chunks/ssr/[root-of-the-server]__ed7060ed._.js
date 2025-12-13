abdfdeb4646c34857f8e158c789650bbcb0bf323
Ketal528 <sutariyaanshuman1413@gmail.com>
2025-12-13 13:04:40 +0530

    TITLE CHANGE

--- .next/server/chunks/ssr/[root-of-the-server]__ed7060ed._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__ed7060ed._.js
@@ -705,7 +705,7 @@ function Hero() {
                     children: [
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("h1", {
                             className: "text-4xl font-headline tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl text-foreground",
-                            children: "Art from the Earth"
+                            children: "Art from the Earth by Anshuman"
                         }, void 0, false, {
                             fileName: "[project]/src/components/hero.tsx",
                             lineNumber: 24,
